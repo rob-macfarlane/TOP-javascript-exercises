f5775406176ec3d6114f15d43770e8034bbc78fa
rob-macfarlane <remacfarlane29@gmail.com>
2022-09-16 09:43:29 -0700

    Update with completed scripts that have been tested

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
+const ftoc = function(tempFarenheit) {
+  return Math.round((tempFarenheit - 32) * 5/9 * 10) / 10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(tempCelsius) {
+  return Math.round(((tempCelsius * 9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
