5cd0fe17ad56183b0a6988f12b9f38c7d6d5c24d
rob-macfarlane <remacfarlane29@gmail.com>
2022-09-16 09:04:39 -0700

    Update files with completed exercise script

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeValues) {
+    for (const value of removeValues) {
+        while (array.includes(value)) {
+            array.splice(array.indexOf(value), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
