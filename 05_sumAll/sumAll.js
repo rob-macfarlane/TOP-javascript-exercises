8578ced5a4f381c389a572ae6b9330a24474eeed
rob-macfarlane <remacfarlane29@gmail.com>
2022-09-16 09:25:53 -0700

    Update with completed and tested script

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (typeof(firstNumber) !== 'number' || typeof(secondNumber) !== 'number' || firstNumber * secondNumber < 0) {
+        return 'ERROR'
+    } else {
+        let x = 0;
+        for (i=Math.min(firstNumber, secondNumber); i<Math.max(firstNumber, secondNumber) + 1; i++) {
+            x += i;
+        }
+        return x;
+    }
+    
 };
 
 // Do not edit below this line
