021a34150906ce3821a542545a79f6bac1562b90
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-27 17:14:30 -0800

    Updates calculator script to completion

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
+const add = function(numberOne, numberTwo ) {
+	return numberOne + numberTwo;
+};
+
+const subtract = function(numberOne, numberTwo) {
+  return numberOne - numberTwo;
 	
 };
 
-const subtract = function() {
+const sum = function(numbers) {
+	count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    count += numbers[i];
+  }
+  return count;
+};
+
+const multiply = function(numbers) {
+  total = 1
+  for (let i = 0; i < numbers.length; i++) {
+    total *= numbers[i];
+  }
+  return total;
+};
+
+const power = function(numberOne, numberTwo) {
+  return numberOne ** numberTwo;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	total = 1;
+  for (let i = 1; i <= number; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
