9729d5551e3a9ff0f57b4131eb7a3aa30555237d
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-27 20:26:23 -0800

    Completed Get the Titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let justTitles =[];
+    for (i =0; i < array.length; i++) {
+        justTitles.push(array[i].title);
+    }
+    return justTitles;
 };
 
 // Do not edit below this line
