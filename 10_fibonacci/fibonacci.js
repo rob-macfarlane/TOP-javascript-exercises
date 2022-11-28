4a7db57218e08cc83d57df3e620387c218a4b3f7
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-27 19:59:17 -0800

    Update fibonacci with solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(memberNumber) {
+    if (memberNumber < 1) {
+        return 'OOPS'
+    }
+    memberNumber = parseInt(memberNumber)
+    if (memberNumber === 1 || memberNumber === 2) {
+        return 1;
+    } else 
+        return fibonacci(memberNumber - 1)  + fibonacci(memberNumber - 2)
+    
 };
 
 // Do not edit below this line
