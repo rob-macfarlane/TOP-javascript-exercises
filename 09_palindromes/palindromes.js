34a6003f978e9dd777ee157a41ac445f141f925d
rob-macfarlane <remacfarlane29@gmail.com>
2022-11-27 19:14:32 -0800

    Changes to palindrome, but incomplete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let inputArray = inputString.split("");
+    let testArray = inputArray.reverse()
+    
+    if (testArray.toString() == inputArray.toString()) {
+        return true;
+    }
 };
 
 // Do not edit below this line
